a3937e5dd715dcf7c3823b7e7c709c85da42b655
David Magalhães <david.magalhaes90@hotmail.com>
2019-10-04 15:36:04 -0300

    Finalizações

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class ImportedProduct extends Product{
+
+	Double customFee;
+
+	public ImportedProduct(String name, Double price, Double customFee) {
+		super(name, price);
+		this.customFee = customFee;
+	}
+
+	public Double getCustomFee() {
+		return customFee;
+	}
+
+	public void setCustomFee(Double customFee) {
+		this.customFee = customFee;
+	}
+	
+	public double totalPrice() {
+		return getPrice() + customFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return getName() 
+				+ " $ " 
+				+ String.format("%.2f", totalPrice())
+				+ " (Customs fee: $ " 
+				+ String.format("%.2f", customFee)
+				+ ")";
+	}
+	
+}
